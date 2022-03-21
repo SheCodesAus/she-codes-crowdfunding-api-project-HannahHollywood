5ca6743ffaa0a00e0ada87ae6e6e67445d6b80d5
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-03-21 20:07:03 +0800

    Added status codes to the responses (Step 11)

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 from rest_framework import status
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
@@ -36,11 +37,21 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            return Response(serializer.data)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 class ProjectDetail(APIView):
     def get_object(self, pk):
-        return Project.objects.get(pk=pk)
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
 
     def get(self, request, pk):
         project = self.get_object(pk)
