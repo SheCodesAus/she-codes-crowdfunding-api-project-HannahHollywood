56bdbbf6cbbdeb41bd1d7a624904463617141c72
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-03-22 17:28:11 +0800

    Updated Permissions & Authentication settings

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 
@@ -28,6 +29,7 @@ class PledgeList(APIView):
         )
 
 class ProjectList(APIView):
+
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -47,9 +49,17 @@ class ProjectList(APIView):
         )
 
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request,project)
+            return project
+            
         except Project.DoesNotExist:
             raise Http404
 
@@ -57,3 +67,15 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+        # When you view a specific project there will now be a “put” option
